171074b8d9e77ca004d07e83ec647ac9bf5ed0e4
nawodyaishan <nawodyain@gmail.com>
2022-10-22 23:47:05 +0530

    Ad js test

--- tests.js
+++ tests.js
@@ -6,5 +6,6 @@ describe('getCelsius', () => {
         expect(getCelsius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
         expect(getCelsius([-58, -22, -4, 14])).to.deep.equal([-50, -30, -20, -10])
         expect(getCelsius([104, 122, 158, 176])).to.deep.equal([40, 50, 70, 80])
+        expect(getCelsius([104, 122, 158, 176])).to.deep.equal([40, 50, 70, 80])
     })
 })
